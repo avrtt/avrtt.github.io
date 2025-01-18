da21aecdaeb3f574d2aa452ad80f47e41f08caa0
avrtt <avrtt@tuta.io>
2025-01-18 16:21:16 +0500

    Adventures/Research/Thoughts: add compact views

--- src/pages/main/thoughts.js
+++ src/pages/main/thoughts.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
 import Box from '@mui/material/Box';
@@ -14,11 +14,22 @@ const TITLE = 'Thoughts - avrtt.blog'
 
 const Posts = ({ data }) => {
 	const posts = data.allMdx.nodes;
+	const [isTileView, setIsTileView] = useState(true);
   
 	useEffect(() => {
 	  window.scrollTo(0, 0);
 	}, []);
+
+	const toggleView = () => {
+		setIsTileView(!isTileView);
+	};
   
+	const viewVariants = {
+		hidden: { opacity: 0 },
+		visible: { opacity: 1, transition: { duration: 0.3 } },
+		exit: { opacity: 0, transition: { duration: 0.3 } },
+	};
+
 	return (
 		<motion.div className='noselect'
 			initial={{ opacity: 0 }}
@@ -30,36 +41,68 @@ const Posts = ({ data }) => {
 				<title>{TITLE}</title>
 			</Helmet>
 
+			<div className="viewButtonWrapper">
+				<button onClick={toggleView} className="viewButton noselect">
+					{isTileView ? 'Compact view' : 'Gallery view'}
+				</button>
+			</div>
+
 			<Box>
-				<ImageList cols={2} gap={5}>
-					{posts.map(post => {
-						const image = getImage(post.frontmatter.banner);
-						return (
-							<ImageListItem key={post.id}>
-								<div className="hover">
-									<AnimationOnScroll offset="999999" animateIn="animate__fadeIn" animatePreScroll="false" duration="0.3">
-										<GatsbyImage
-											className="prev-home"
-											image={image}
-											alt={post.frontmatter.title}
-										/>
-									</AnimationOnScroll>
-									<Link to={post.frontmatter.slug}>
-										<div className="overlay-back">
-											<div className="titleblock">
-												<p className="title">{post.frontmatter.title}</p>
-											</div>
+				<motion.div
+					key={isTileView ? 'tileView' : 'listView'}
+					initial="hidden"
+					animate="visible"
+					exit="exit"
+					variants={viewVariants}
+				>
+					{isTileView ? (
+						<ImageList cols={2} gap={5}>
+							{posts.map(post => {
+								const image = getImage(post.frontmatter.banner);
+								return (
+									<ImageListItem key={post.id}>
+										<div className="hover">
+											<AnimationOnScroll offset="999999" animateIn="animate__fadeIn" animatePreScroll="false" duration="0.3">
+												<GatsbyImage
+													className="prev-home"
+													image={image}
+													alt={post.frontmatter.title}
+												/>
+											</AnimationOnScroll>
+											<Link to={post.frontmatter.slug}>
+												<div className="overlay-back">
+													<div className="titleblock">
+														<p className="title">{post.frontmatter.title}</p>
+													</div>
+												</div>
+												<div className="overlay-base">
+													<p className="title">{post.frontmatter.title}</p>
+													<div className="description">{post.frontmatter.desc}</div>
+												</div>
+											</Link>
 										</div>
-										<div className="overlay-base">
-											<p className="title">{post.frontmatter.title}</p>
-											<div className="description">{post.frontmatter.desc}</div>
-										</div>
-									</Link>
-								</div>
-							</ImageListItem>
-						);
-					})}
-				</ImageList>
+									</ImageListItem>
+								);
+							})}
+						</ImageList>
+					) : (
+						<Box component="ul" sx={{ listStyleType: 'none', padding: 0 }}>
+							{posts.map(post => (
+								<li key={post.id} style={{ marginBottom: '4px' }}>
+									<div>
+										<span style={{opacity: "0.5"}}>{post.frontmatter.date}</span> 
+										&nbsp;&nbsp;
+										<Link to={post.frontmatter.slug} className="compactViewLink">
+											{post.frontmatter.titleDetailed || post.frontmatter.title}
+										</Link>
+										&nbsp;&nbsp;
+										<span style={{opacity: "0.5"}}>#{post.frontmatter.index}</span>
+									</div>
+								</li>
+							))}
+						</Box>
+					)}
+				</motion.div>
 			</Box>
 	  	</motion.div>
 	);
@@ -76,7 +119,9 @@ export const query = graphql`
       			frontmatter {
         			index
         			title
+					titleDetailed
 					desc
+					date
 					slug
 					banner {
           				childImageSharp {
