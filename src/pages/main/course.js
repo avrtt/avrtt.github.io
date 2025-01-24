561c2819cde611951ece6b988687cc53b0fea4da
avrtt <avrtt@tuta.io>
2025-01-24 21:01:56 +0500

    Replace number of articles from Course to About

--- src/pages/main/course.js
+++ src/pages/main/course.js
@@ -5,7 +5,6 @@ import { motion } from 'framer-motion';
 import M from '../../components/Markdown';
 import StickerPack from '../../components/StickerPack';
 import { courseLastUpdated } from '../../data/lastUpdated';
-import CountPosts from "../../fetchers/count_posts";
 import 'animate.css/animate.min.css';
 
 const TITLE = 'Course - avrtt.blog'
@@ -58,8 +57,6 @@ const Course = ({ data }) => {
         }
     }
 
-	 const { courseCount } = CountPosts();
-
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
@@ -90,7 +87,6 @@ const Course = ({ data }) => {
 						</div>
 					</div>
 				</div>
-				<p>There are <strong>{courseCount}</strong> articles in the course so far.</p>
 			</div>
 			<br/>
 			<div class="courseBody noselect">
