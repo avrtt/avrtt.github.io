1243535881e5f320cba0f699576d348fb63b4271
avrtt <avrtt@tuta.io>
2025-01-26 22:20:33 +0500

    Minor fix

--- src/components/NotFinishedNotice/index.js
+++ src/components/NotFinishedNotice/index.js
@@ -5,7 +5,7 @@ const NotFinishedNotice = () => {
   return (
 	<>
     	<div class="yellowNotice">
-        	<M text="## 🚧 HEADS UP!"/>
+        	<M text="## 🚧 HEADS UP, A DRAFT!"/>
         	<M text="This post isn't finished. There may be typos, inconsistent narration, superficiality and lack of stylization. If you'd like to contribute, you're always welcome to [make a PR](https://github.com/avrtt/avrtt.github.io)."/>
     	</div>
 	</>
