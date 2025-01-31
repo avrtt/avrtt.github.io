83289f73e52bb60b7c3ea38922007f585d772ebc
avrtt <avrtt@tuta.io>
2025-02-01 01:56:35 +0500

    Add & configure Sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap", 
+    "gatsby-plugin-sass",
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import "../global.css"
+import "../global.scss"
 import Home from './main/home'
 
 const App = () => {
