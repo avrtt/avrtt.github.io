daf4c31596e49b10416e6875b8e33af1e242bef9
avrtt <avrtt@tuta.io>
2025-02-03 13:32:57 +0500

    GoalCategory: change style imports

--- src/components/GoalCategory/index.js
+++ src/components/GoalCategory/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Goal from '../Goal'
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import M from '../Markdown';
+import * as stylesGoalsPage from "../../styles/goals_page.module.scss";
 
 const descStyle = {
     'text-align': 'center',
@@ -22,9 +23,9 @@ const GoalCategory = ({ array, name, desc='', isOpaque, hideChecked, hideUncheck
     <br/>
     <div class="sectionBreak"><M text="----------"/></div>
     <AnimationOnScroll offset="300" duration="1.2" animateIn="animate__fadeIn" animateOnce="true">
-		<div class="goalsCategoryTitle">{name}</div>
+		<div class={stylesGoalsPage.goalsCategoryTitle}>{name}</div>
 		<div class="noselect" style={descStyle}>{desc}</div>
-		<table class="goalCategoryTable" className="table table-striped table-bordered">
+		<table class={stylesGoalsPage.goalCategoryTable} className="table table-striped table-bordered">
             <tbody>
                 {array && array.map(goal =>        
                     <Goal goal={goal} isOpaque={isOpaque} hideChecked={hideChecked} hideUnchecked={hideUnchecked}/>
