e3e23cb554d3fa2e94714b7f6f48aebcc98cdfd4
avrtt <avrtt@tuta.io>
2025-02-07 23:28:41 +0500

    OffensiveNotice: add burning animation & minor fixes

--- src/components/OffensiveNotice/index.js
+++ src/components/OffensiveNotice/index.js
@@ -1,15 +1,22 @@
 import React from "react"
 import M from "../Markdown"
+import B from "../Burn"
 import StickerPack from "../../components/StickerPack"
 import * as stylesPlaques from "../../styles/plaques.module.scss"
 
 const OffensiveNotice = () => {
   return (
 	<>
-    	<div class={stylesPlaques.greyNoticeCompact}>
-        	<h2><StickerPack sticker="pepe_punch" marginLeft="0px" marginRight="0.2em"/> First time?</h2>
-        	<M text="This page may be offensive to some people. It's probably filled with satire to scare off the wrong crowd and attract the right one, or just to make some lulz out of it. Sensitive individuals are advised to go fuck themselves right now. 🖕❤️"/>
-    	</div>
+		<B
+			fireSize = {[16, 30]}
+			fireDuration = {1000}
+			fireFrequency = {300}
+		>
+			<div class={stylesPlaques.greyNoticeCompact}>
+				<h2><StickerPack sticker="pepe_punch" marginLeft="0px" marginRight="0.2em"/> <span style={{ color: "red", fontWeight: "66666" }}>OFFENSIVENESS</span></h2>
+				<M text="This page may be offensive to some people. It's probably filled with satire to scare off the wrong crowd and attract the right one, or just to make some lulz out of it. Sensitive individuals are advised to [go fuck themselves](/dev/null) right now. 🖕❤️"/>
+			</div>
+		</B>
 	</>
   );
 };
