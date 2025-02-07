874869c93014e57f23429d3f51690d9f582f5606
avrtt <avrtt@tuta.io>
2025-02-07 23:24:32 +0500

    Minor fixes

--- src/components/AnomalyNotice/index.js
+++ src/components/AnomalyNotice/index.js
@@ -8,8 +8,7 @@ const AnomalyNotice = () => {
     	<div class={stylesPlaques.yellowNotice}>
         	<M text="## ATTENTION!"/>
         	<M text="This page is classified as a strangely suspicious web document having unexpected characteristics within an appropriate environment. I don't really know how you came across it, but I deliberately left it contained in the quarantine zone to collect records of anomalous activity."/>
-			<br/>
-			<M text="Facility information was omitted purposefully. Crawlers are active."/>
+			<M text="Facility information was omitted purposefully."/>
     	</div>
 	</>
   );
--- src/components/MultilingualNotice/index.js
+++ src/components/MultilingualNotice/index.js
@@ -8,7 +8,7 @@ const MultilingualNotice = () => {
 	<>
     	<div class={stylesPlaques.greyNoticeCompact}>
         	<h2><StickerPack sticker="babel_tower" marginLeft="0px" marginRight="0.2em"/> Забугорный слогъ</h2>
-        	<M text="This page is written in multiple languages. It's either not yet translated or written so to convey the message accurately. I don't know why you should be warned about this. I just love these custom Wikipedia-like plaques."/>
+        	<M text="This page is written in multiple languages. It's either not yet translated or written so to convey the message accurately. I don't know why you should be warned about this. I just love these custom wiki-like plaques."/>
     	</div>
 	</>
   );
