df63de966ddbbcab68f08ef7418e60c6c5f26aea
avrtt <avrtt@tuta.io>
2025-02-11 22:33:25 +0500

    Tooltip: remove hover & add click behavior for small screens

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,15 +1,36 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react";
 import info from "../../images/goals/info.svg"
 import * as styles from "./styles.module.scss"
 
 const Tooltip = ({ text }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const tooltipRef = useRef(null);
+
+    const handleIconClick = (e) => {
+        e.stopPropagation();
+        setIsOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (tooltipRef.current && !tooltipRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <span className={styles.tooltipGeneral}>
-            <img id={styles.infoGeneral} src={info} alt='info'/>
-            <span className={styles.tooltiptextGeneral}>
+        <span className={styles.tooltipGeneral} ref={tooltipRef}>
+            <img id={styles.infoGeneral} src={info} alt='info' onClick={handleIconClick}/>
+            <span className={isOpen ? `${styles.tooltiptextGeneral} ${styles.visible}` : styles.tooltiptextGeneral}>
                 {text}
             </span>
         </span>
     );
 };
+
 export default Tooltip;
