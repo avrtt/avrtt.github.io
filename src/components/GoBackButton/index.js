3748fc87e2eade094204eaf3d62d1951ce0a88fc
avrtt <avrtt@tuta.io>
2025-02-16 20:04:12 +0500

    GoBackButton: rewrite code, fix bugs, make reusable, add customization props, responsiveness & local styles

--- src/components/GoBackButton/index.js
+++ src/components/GoBackButton/index.js
@@ -1,90 +1,20 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 import Arrow from '../../images/freelance/services/arrow1.svg';
+import * as styles from './styles.module.scss';
 
-const GoBackButton = ({ link="/" }) => {
-  const [scrolled, setScrolled] = useState(false);
-  const buttonRef = useRef(null);
-  const initialTopOffset = 12;
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const threshold = 6 + initialTopOffset;
-
-      if (window.scrollY >= threshold && !scrolled) {
-        setScrolled(true);
-      } else if (window.scrollY < threshold && scrolled) {
-        setScrolled(false); 
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [scrolled]);
-
+const GoBackButton = ({ link = '/', title = 'Back to the main page' }) => {
   return (
-    <div
-      className={`goBackWrapper ${scrolled ? 'scrolled' : ''}`}
-      style={!scrolled ? { top: `${initialTopOffset}vh` } : {}}
-      ref={buttonRef}
-    >
-      <Link title="Back to the main page" className="goBack noselect" to={link}>
-        <div className="goBack noselect">
-          <img src={Arrow} alt="button_icon" width="50%" height="100%" />
-        </div>
+    <div className={styles.goBackWrapper}>
+      <Link
+        to={link}
+        className={styles.goBack}
+        title={title}
+      >
+        <img src={Arrow} alt="button_icon" width="50%" height="100%" />
       </Link>
     </div>
   );
 };
 
 export default GoBackButton;
-
-
-
-/*import React, { useEffect, useState, useRef } from 'react';
-import { Link } from 'gatsby';
-import Arrow from '../../images/freelance/services/arrow.svg';
-
-const GoBackButton = () => {
-  const [scrolled, setScrolled] = useState(false);
-  const buttonRef = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (buttonRef.current) {
-        const buttonTop = buttonRef.current.getBoundingClientRect().top;
-
-        if (buttonTop <= 20) {
-          setScrolled(true);
-        } else {
-          setScrolled(false);
-        }
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  return (
-    <div
-      className={`goBackWrapper ${scrolled ? 'scrolled' : ''}`}
-      ref={buttonRef}
-    >
-      <Link title="Back to the main page" className="goBack noselect" to="/freelance">
-        <div className="goBack noselect">
-          <img src={Arrow} alt="button_icon" width="50%" height="100%" />
-        </div>
-      </Link>
-    </div>
-  );
-};
-
-export default GoBackButton;
-*/
