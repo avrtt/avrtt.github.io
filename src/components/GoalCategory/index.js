a245590debf5551ee24df47970b6584526a65fd0
avrtt <avrtt@tuta.io>
2025-02-16 21:21:38 +0500

    GoalCategory: smooth filtering animation #1303e15

--- src/components/GoalCategory/index.js
+++ src/components/GoalCategory/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Goal from '../Goal'
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { motion, AnimatePresence, LayoutGroup } from 'framer-motion';
 import M from '../Markdown';
 import * as stylesGoalsPage from "../../styles/goals_page.module.scss";
 
 const descStyle = {
     'text-align': 'center',
-    'font-size': '22px',
-    'margin-bottom': '32px',
-    'margin-top': '-55px',
+    'font-size': 'clamp(1rem, 2.5vw, 1.4rem)',
+    'line-height': 'clamp(1.2rem, 2.5vw, 1.8rem)',
+    'margin-bottom': '2rem',
+    'margin-top': '-3rem',
     'font-weight': 'lighter'
 }
 
@@ -17,24 +19,50 @@ const sectionEndStyle = {
 }
 
 const GoalCategory = ({ array, name, desc='', isOpaque, hideChecked, hideUnchecked}) => {
+
+  const filteredGoals = array.filter(goal => {
+    if (hideChecked && goal.status === 'c') return false;
+    if (hideUnchecked && goal.status === 'u') return false;
+    return true;
+  });
+
   return (
     <>
-  
-    <br/>
-    <div class="sectionBreak"><M text="----------"/></div>
-    <AnimationOnScroll offset="300" duration="1.2" animateIn="animate__fadeIn" animateOnce="true">
-		<div class={stylesGoalsPage.goalsCategoryTitle}>{name}</div>
-		<div class="noselect" style={descStyle}>{desc}</div>
-		<table class={stylesGoalsPage.goalCategoryTable} className="table table-striped table-bordered">
-            <tbody>
-                {array && array.map(goal =>        
-                    <Goal goal={goal} isOpaque={isOpaque} hideChecked={hideChecked} hideUnchecked={hideUnchecked}/>
-                )}
-            </tbody>
-        </table>
+      <br/>
+      <div className="sectionBreak"><M text="----------"/></div>
+      <AnimationOnScroll offset="300" duration="1.2" animateIn="animate__fadeIn" animateOnce="true">
+        <div className={stylesGoalsPage.goalsCategoryTitle}>{name}</div>
+        <div className="noselect" style={descStyle}>{desc}</div>
+        <LayoutGroup>
+          <motion.table 
+            className={`${stylesGoalsPage.goalCategoryTable} table table-striped table-bordered`}
+            layout
+          >
+            <motion.tbody layout>
+              <AnimatePresence>
+                {filteredGoals.map(goal => (
+                  <motion.tr
+                    key={goal.text}
+                    layout
+                    initial={{ opacity: 0, y: -8 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 8 }}
+                    transition={{ duration: 0.2 }}
+                  >
+                    <Goal
+                      goal={goal}
+                      isOpaque={isOpaque}
+                      hideChecked={hideChecked}
+                      hideUnchecked={hideUnchecked}
+                    />
+                  </motion.tr>
+                ))}
+              </AnimatePresence>
+            </motion.tbody>
+          </motion.table>
+        </LayoutGroup>
         <p style={sectionEndStyle}></p>
-    </AnimationOnScroll>
-    
+      </AnimationOnScroll>
     </>
   );
 };
