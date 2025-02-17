bce0fb39e8d2b30226528e3645f9530e93226e31
avrtt <avrtt@tuta.io>
2025-02-18 04:17:10 +0500

    SEO: add global metadata, sitemap data fetching & robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,61 @@
 module.exports = {
   siteMetadata: {
     title: `avrtt.blog`,
-    siteUrl: `https://avrtt.github.io`
+    description: `My personal all-in-one corner of the internet — a reinvention of old school blogging, community hub, info page, data science & AI learning platform.`,
+    image: `/src/images/icon.png`,
+    siteUrl: `https://avrtt.github.io`,
+    siteName: `avrtt.blog — notes of a lifelong explorer`,
+    author: `Vladislav Averett`,
+    twitterUsername: `@vladaverett`,
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap", 
-    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        query: `
+          {
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+            allMdx {
+              nodes {
+                frontmatter {
+                  slug
+                  prioritySitemap
+                  changefreqSitemap
+                }
+              }
+            }
+          }
+        `,
+        serialize: ({ path, node }) => {
+          return {
+            url: `https://avrtt.github.io${path}`,
+            changefreq: node?.frontmatter?.changefreqSitemap || "weekly",
+            priority: parseFloat(node?.frontmatter?.prioritySitemap) || 0.4,
+          }
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://avrtt.github.io',
+        sitemap: 'https://avrtt.github.io/sitemap-index.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
         "icon": "src/images/icon.png"
       }
     }, 
+    "gatsby-plugin-image", 
     "gatsby-plugin-sharp", 
+    "gatsby-plugin-sass",
     "gatsby-transformer-sharp", 
     {
       resolve: 'gatsby-source-filesystem',
