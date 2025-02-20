3d76abca18e3b5f58184474ac5e0b43c7e5d6a6e
avrtt <avrtt@tuta.io>
2025-02-21 02:41:27 +0500

    FreelanceCategory: move styles to local scss module

--- src/components/FreelanceCategory/index.js
+++ src/components/FreelanceCategory/index.js
@@ -1,44 +1,20 @@
 import React, { useEffect } from "react";
 import { Link } from "gatsby"
 import * as stylesFreelancePages from "../../styles/freelance_pages.module.scss"
-
-const categoryNameStyle = {
-  'font-weight': 'bold',
-  'margin-left': '14px',
-}; 
-
-const numProjectsStyle = {
-  'font-weight': 'lighter',
-  'font-size': '18px',
-  'float': 'right',
-  'line-height': '46.5px',
-}; 
-
-const imgStyle = {
-  'vertical-align': 'middle',
-  'margin-right': '2px',
-}; 
-
-const categoryDescStyle = {
-  'font-weight': 'lighter',
-  'font-size': '18px',
-  'margin-left': '14px',
-  'margin-top': '32px',
-  'width': '80%'
-}; 
+import * as styles from "./styles.module.scss"
 
 const FreelanceCategory= ({ categoryName, categoryDesc, numProjects, img, url }) => {
   
   return (
-    <Link to={url}><div class={stylesFreelancePages.freelanceCategoryWrapper}>
-      <span style={categoryNameStyle}>{categoryName}</span>
-      <span style={numProjectsStyle}>
-        {numProjects}
+    <Link to={url}><div className={stylesFreelancePages.freelanceCategoryWrapper}>
+      <span className={styles.categoryName}>{categoryName}</span>
+      <span className={styles.rightUpperCornerWrapper}>
+        <span className={styles.numProjects}>{numProjects}</span>
         &nbsp;&nbsp;&nbsp;&nbsp;
-        <img src={img} class="noselect" alt="category_icon" width="60" height="60" style={imgStyle} />
+        <img src={img} className={`${styles.img} noselect`} alt="category_icon" width="60" height="60"/>
       </span>
       <br/>
-      <p style={categoryDescStyle}>{categoryDesc}</p>
+      <p className={styles.categoryDesc}>{categoryDesc}</p>
     </div></Link>
   )
 };
