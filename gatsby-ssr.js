7f05e02aa0fa7df7cdfc6734b739bb3d8a306fca
avrtt <avrtt@tuta.io>
2025-02-21 02:17:51 +0500

    Add server-side rendeing for general meta tags

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import Layout from './src/components/layout';
 
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta key="charset" charSet="utf-8" />,
+    <meta
+      key="viewport"
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    />,
+  ]);
+};
+
 export const wrapPageElement = ({ element, props }) => {
   // prevent undefined pages from wrapping twice since 404.js redirects to NotFound component
   if (props.pageContext?.is404) {
