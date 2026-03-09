deae1af2112de1b0feb2a3d992b77e88e5139806
avrtt <avrtt@tuta.io>
2026-03-09 09:52:50 +0300

    CV: change position name

--- src/pages/main/cv.tsx
+++ src/pages/main/cv.tsx
@@ -42,7 +42,7 @@ const CV = () => {
 			<span className="mobileOnlySupport"><br/></span>
 			<div className="noselect" style={downloadStyle}><a href="https://github.com/avrtt/avrtt.github.io/blob/main/src/documents/cv.pdf">Download</a> <br/> <div className={"desktopOnlySupport"} style={downloadDescStyle}>(compact version)</div></div>
 
-			<p>I'm a <b>Middle Data Scientist</b> with experience in ML engineering, Python programming and analytics. I specialize in designing/implementing/refining/deploying ML systems, data-related problems (collection, processing, transforming, modeling, visualization, analysis, interpreting), and developing various automation software.</p>
+			<p>I'm a <b>Middle ML engineer</b> with experience in data science (analytics) and software engineering. I specialize in designing/implementing/refining/deploying ML systems, data-related problems (collection, processing, transforming, modeling, visualization, analysis, interpreting), and developing various automation software.</p>
 			<p>This page provides a more detailed description of my professional skills, as opposed to the minimal version of the CV provided in the link above. For my freelance services and projects, check out the <Link to="/freelance">Freelance</Link> page.</p>
 
 			<div className="sectionBreak"><M text="----------"/></div>
@@ -64,7 +64,7 @@ const CV = () => {
 				pointLast={"- Developed reusable components that combined LLM reasoning with vision/audio modules. These components were reused across multiple product features and halved the development time for new scenarios. In particular, applying this solution shortened prototyping cycles, improved contextual accuracy in target features and sped up generation of analytic reports from corporate data (tasks that previously took hours or days now completed in minutes). Integrated agentic workflows (tool-calling, function execution, planners) using LlamaIndex–style abstractions with custom orchestration logic for multi-step reasoning tasks."}
 			/>
 			<CvPosition 
-				positionName={"Data Scientist, ML Engineer & Python Developer"} 
+				positionName={"Data Scientist & Python Developer"} 
 				companyName={"Freelance"} 
 				companyLink={"/freelance"}
 				emoji={"🥥"} 
